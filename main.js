23c9b88a6818e042059ef8c8e5b1b1754641f729
jamesfowler46 <jamesfowler1000@gmail.com>
2016-11-27 16:55:23 -0800

    memorygame
    Was hard for me but i worked hard

---
+++ main.js
@@ -0,0 +1,66 @@
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
+
+if (cardOne === cardTwo) {
+	alert("You found a match!.");
+}if (cardOne === cardThree) {
+	alert("Sorry, try again.");
+}if (cardOne === cardFour) {
+	alert("Sorry, try again.");
+}
+
+
+if (cardTwo === cardOne) {
+	alert("You found a match!.");
+}if (cardTwo === cardThree) {
+	alert("Sorry, try again.");
+}if (cardTwo === cardFour) {
+	alert("Sorry, try again.");
+}
+
+
+if (cardThree === cardFour) {
+	alert("You found a match!.");
+}if (cardThree === cardOne) {
+	alert("Sorry, try again.");
+}if (cardThree === cardTwo) {
+	alert("Sorry, try again.");
+}
+
+
+if (cardFour === cardThree) {
+	alert("You found a match!.");
+}if (cardFour === cardOne) {
+	alert("Sorry, try again.");
+}if (cardFour === cardTwo) {
+	alert("Sorry, try again.");
+}
+
+var cardsArray = [queen, queen, king, king.];
+
+var cardsInPlay = 
+			for (var i = 0; i < cards.length; i++) {
+	cardElement.setAttribute('data-card', cards[i]);
+	}		
+			for (var i=0; i<cards.length; i++) {
+	cardElement.addEventListener('click', isTwoCards)	
+	}
+var inMatch = function isTwoCards() {
+  	cardsInPlay.push(this.getAttribute('data-card'));
+  	if (cardsInPlay.length === 2) {
+    isMatch(cardsInPlay);
+    cardsInPlay = [];
+	}
+}
+
+
+document.getElementById('trogdor');
+
+var strongbadia = document.createElement('section');
+strongbadia.className = 'blue-skies';
+world.appendChild(strongbadia);
+
+
